4393c7e671a2ae57925b24997dabfea5e1130a72
Rolf Eleveld <Rolf.Eleveld@outlook.com>
2022-09-19 17:59:14 +0400

    edits of class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        String studentFirstName = "Harry";
+        String studentLastName = "Belafonte";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+        boolean hasPerfectAttendance = true;
+        
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
+        System.out.println("What should GPA be?");
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
         
     }
 }
