3aa4fc57ff64322c993630d5f286e803dd97ba2d
Rolf Eleveld <Rolf.Eleveld@outlook.com>
2022-09-19 18:12:42 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
+        int inputtedNum = 1;
+        while (inputtedNum != 0) {
+            System.out.println("Pick a number between 1 and 10");
+            Scanner scanner = new Scanner(System.in);
+            inputtedNum = scanner.nextInt();
 
-        int inputtedNum = scanner.nextInt();
+            if (inputtedNum < 5) {
+                System.out.println("good luck");
+            } else {
+                System.out.println("Choose shoe");
+            }
+        }
     }
 }
