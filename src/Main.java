97eeebf6994838320a72a378336507d139671346
Rolf Eleveld <Rolf.Eleveld@outlook.com>
2022-09-19 19:15:05 +0400

    functions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
+    public static double annualSalary(double hoursPerWeek, double hourlyRate){
+        double result = hoursPerWeek * 52 * hourlyRate;
+        return result; 
+    }
     public static void main(String[] args) {
-        
+        double earn = annualSalary(1660.3, 3.65);
+        System.out.println(earn);
     }
 
 }
